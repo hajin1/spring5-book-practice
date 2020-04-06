27cdad31456dddfb049c337350025772db945277
hajin <hajin@synapsoft.co.kr>
2020-04-06 21:32:56 +0900

    spring1 Chapter3 - 1~5장 실습

---
+++ src/main/java/chap03/assembler/Assembler.java
@@ -0,0 +1,32 @@
+package chap03.assembler;
+
+import chap03.spring.ChangePasswordService;
+import chap03.spring.MemberDao;
+import chap03.spring.MemberRegisterService;
+
+public class Assembler {
+    private MemberDao memberDao;
+    private MemberRegisterService regSvc;
+    private ChangePasswordService pwdSvc;
+
+    public Assembler() {
+        memberDao = new MemberDao();
+        regSvc = new MemberRegisterService(memberDao);
+        pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao);
+
+    }
+
+    public MemberDao getMemgerDao() {
+        return memberDao;
+    }
+
+    public MemberRegisterService getMemberRegisterService() {
+        return regSvc;
+    }
+
+    public ChangePasswordService getChangePasswordService() {
+        return pwdSvc;
+    }
+
+}
